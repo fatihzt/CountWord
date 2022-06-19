e7a161605b71593fa0402d4fe96fcd2db3899a32
fatihzt <muhammetfatihozata7@gmail.com>
2022-06-19 11:44:39 +0300

    Add project files.

---
+++ CountWord/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class CountingWord{         
+    static void Main(string[] args)
+    {
+
+         char space = ' ';
+         Console.WriteLine("Please enter a string");
+         string str1=Convert.ToString(Console.ReadLine());
+
+         int counting = str1.Count(f => (f == ' '));
+         Console.WriteLine(counting+1);
+         
+    }
+}
